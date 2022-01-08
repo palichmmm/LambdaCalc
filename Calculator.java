6265dd5cc1fbc8d5970622d547e153b5ee3f6259
Евгений Козловский <palichmmm@gmail.com>
2022-01-08 09:59:52 +0300

    Исправления решения задачи

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,16 @@ public class Calculator  {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        // отлавливаем исключение
+        try {
+            return x / y;
+        } catch (ArithmeticException err) {
+            System.out.println(x + " / " + y + " = Ошибка! Деление на ноль!!! " + err);
+            return 0;
+        }
+        // или второе решение: проверяем (y) на ноль и выводим сообщение об ошибке
+    };
     Predicate<Integer> isPositive = x -> x > 0;
     Consumer<Integer> println = System.out::println;
 }
